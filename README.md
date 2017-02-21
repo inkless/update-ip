0915314e51ca65703c63d61a6860516128e68174
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2017-02-20 16:10:55 -0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+update ip
+=========
+
+A small script to update ip.
+
+## Usage
+```bash
+npm start ip_name
+```
+
+## crontab
+```
+*/10 * * * * cd /project/path && /path/to/npm start ip_name
+```
+
---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const request = require('request');
+const CHECK_IP_URL = 'http://ipinfo.io/ip';
+
+const name = process.argv[2];
+if (!name) {
+  console.error('Second param needed.');
+  process.exit(1);
+}
+const updateIpUrl = process.env.UPDATE_IP_URL_BASE + name;
+
+request(CHECK_IP_URL, function(error, response, body) {
+  if (!error && response.statusCode === 200 && body) {
+    updateHomeIp(body.trim());
+  }
+});
+
+function updateHomeIp(ip) {
+  request.post({
+    url: updateIpUrl,
+    form: { ip },
+    auth: {
+      user: process.env.AUTH_NAME,
+      pass: process.env.AUTH_PWD,
+      sendImmediately: true,
+    },
+  }, function(error, response, body) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(body);
+    }
+  });
+}
